dfe366eceafaf96fb5ebd8ca925e24ab3a659a25
carevalo-dev <christian.arevalo@uniquitos.edu.pe>
2023-06-17 13:03:47 -0500

    agregando iconos de elements-plus

--- src/main.js
+++ src/main.js
@@ -7,7 +7,13 @@ import 'element-plus/dist/index.css'
 import router from '@/router'
 import App from './App.vue'
 
+//importamos iconos del elemento plus
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 const app = createApp(App)
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
 
 app.use(ElementPlus)
 app.use(router)
