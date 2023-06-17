04606834fbd4b43ac42b900f4c66702e7d061da7
carevalo-dev <christian.arevalo@uniquitos.edu.pe>
2023-06-17 13:01:11 -0500

    configurado vue-router

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import './style.css'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 //fin de la importacion de la libreria
+import router from '@/router'
 import App from './App.vue'
 
 const app = createApp(App)
 
 app.use(ElementPlus)
+app.use(router)
 app.mount('#app')
