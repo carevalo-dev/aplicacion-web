6dd4c059bf39ac119dc297c7213b83d0d6ac4061
carevalo-dev <christian.arevalo@uniquitos.edu.pe>
2023-06-17 13:26:31 -0500

    agregando comentarios

--- src/main.js
+++ src/main.js
@@ -3,14 +3,16 @@ import './style.css'
 //importar la libreria de element-plus
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-//fin de la importacion de la libreria
+//importamos vue-router
 import router from '@/router'
+//importamos el componente principal
 import App from './App.vue'
 
 //importamos iconos del elemento plus
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 const app = createApp(App)
 
+//configuramos los iconos
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
   }
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,18 @@
 //importamos vue-router
 import { createRouter, createWebHistory } from 'vue-router'
-
+//importamos los componentes de las vistas
 import Home from '@/views/Home.vue'
 
+//creamos las rutas
 const routes = [
     {path: '/', name: 'Home', component: Home},
 ]
 
+//creamos el router
 const router = createRouter({
     history: createWebHistory(),
     routes,
 })
 
+//exportamos el router
 export default router
